098bd2e468ab1058a881d11e3ba7dcc3cd375534
Ben P <ben@g.megatron.org>
2013-06-03 22:47:30 -0600

    w?

--- recipes/dash.rb
+++ recipes/dash.rb
@@ -7,7 +7,7 @@
 include_recipe "riemann::server"
 include_recipe "riemann::client"
 
-%{riemann-dash}.each do |pkg|
+%w{riemann-dash}.each do |pkg|
   gem_package pkg do
     action [:install]
   end
